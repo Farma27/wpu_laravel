23203e40fe728eca38c0580f3417d02e38e304f7
Farma27 <stevenbold60@gmail.com>
2020-08-24 20:25:02 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title', 'Web Progamming Farma')
+
+@section('container')
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-3">Home</h1>
+            </div>
+        </div>
+    </div>
+@endsection
